27de4ff91d039a57fb9f86daa4f4976f7d2968f4
ApoorvVerma98 <126326376+ApoorvVerma98@users.noreply.github.com>
2023-05-16 18:11:19 +0530

    2 files modified

--- src/TemperatureControl.jsx
+++ src/TemperatureControl.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const TemperatureControl = ({
-  temperature,
   incrementTemperature,
   decrementTemperature,
 }) => {
   return (
     <div className="temperature-controls">
-      <button onClick={decrementTemperature}>Decrease</button>
-      <button onClick={incrementTemperature}>Increase</button>
+      <button onClick={decrementTemperature}>-</button>
+      <button onClick={incrementTemperature}>+</button>
     </div>
   );
 };
